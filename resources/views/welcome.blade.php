e06d4bcdea345fb4896447ef101f203809810f06
ReVaNNNNN <revannn5480@gmail.com>
2018-09-02 23:10:52 +0200

    Commit checking connection between local and server.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Order the Dinner :)
                 </div>
 
                 <div class="links">
